6b9f190c9db54f7965b9dddbbc31f88469d78cb2
zdale2 <zdale2@illinois.edu>
2018-10-31 20:50:29 -0500

    Front end progress

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 
-from content_management import Hotels
+from content_management import Hotels, Airbnbs
 
 HOTEL_DICT = Hotels()
+AIRBNB_DICT = Airbnbs()
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return render_template("search-page.html", HOTEL_DICT=HOTEL_DICT)
+	return render_template("search-page.html", HOTEL_DICT=HOTEL_DICT, AIRBNB_DICT=AIRBNB_DICT)
 
 
 @app.route('/profile-page')
--- content_management.py
+++ content_management.py
@@ -1,7 +1,15 @@
 def Hotels():
-	HOTEL_DICT = {  "Hotels" : [[]]    }
+	HOTEL_DICT = {  "1" : ["Crowne Plaza Chicago West Loop", "158", "https://images.trvl-media.com/hotels/2000000/1300000/1293300/1293276/1293276_185_b.jpg"],
+					"2": ["Hotel Julian", "229", "https://images.trvl-media.com/hotels/27000000/26920000/26910700/26910691/9c357764_b.jpg"],
+					"3": ["La Quinta Inn & Suites Chicago Downtown", "199", "https://images.trvl-media.com/hotels/3000000/2510000/2506500/2506471/10256890_b.jpg"] 
+	}
 
-
-	["Hotel Name 1", "200"], 
-					"2" : ["Hotel Name 2", "200"] }
 	return HOTEL_DICT
+
+
+def Airbnbs():
+	AIRBNB_DICT = {  "1": ["Unwind in Style Steps from the Magnificent Mile", "250", "https://a0.muscache.com/4ea/air/v2/pictures/7788b338-d05c-4c96-bced-1a90b549d3a5.jpg?t=r:w1200-h720-sfit,e:fjpg-c90"],
+					 "2": ["Explore Pilsen from This Charming Condo Apartment", "105", "https://a0.muscache.com/4ea/air/v2/pictures/5fc9f2c6-271a-45b5-bdc9-807edeefbd87.jpg?t=r:w1200-h720-sfit,e:fjpg-c90"],
+					 "3": ["Segall Suite near Mccormick Place", "192", "https://a0.muscache.com/4ea/air/v2/pictures/2a2011c9-678c-4157-b281-84fb6ed21e3e.jpg?t=r:w1200-h720-sfit,e:fjpg-c90"]
+	}
+	return AIRBNB_DICT
